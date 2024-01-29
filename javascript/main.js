b2e03a3f1e42e3f9a21c5e586272c147f13f6a15
supersiem <100954549+supersiem@users.noreply.github.com>
2024-01-29 16:31:51 +0100

    javascript update V2.0.8

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,23 @@
 window.addEventListener('DOMContentLoaded', function() {
     console.log("DOM loaded");
-    duplicateAndModify("https://www.example.com", "ytyfyfyftyf","fa-cloud");
-    duplicateAndModify("https://www.example.com", "hallo","fa-thumbs-up");
+
+    setup()
+    make_tiles()
+    //duplicateAndModify("https://www.example.com", "ytyfyfyftyf","fa-cloud");
+    //duplicateAndModify("https://www.example.com", "hallo","fa-thumbs-up");
     remove_the_placeholder();
 });
+function make_tiles() {
+    data = get_JSON();
+    console.log("loading tiles");
+    for (let i = 0; i < data.naam.length; i++) {
+        console.log("loading "+data.naam[i])
+        duplicateAndModify(data.link[i],data.naam[i],data.icon.icondata[i],data.icon.icontype[i])
+    }
+    remove_the_placeholder();
 
-function duplicateAndModify(link, text, icon) {
+}
+function duplicateAndModify(link, text, icondata, icontype) {
     console.log(link);
 
     // Get the original tile
@@ -13,16 +25,15 @@ function duplicateAndModify(link, text, icon) {
 
     // Clone the original tile, including its children
     var clonedTile = originalTile.cloneNode(true);
-
+    clonedTile.id = link
     // Change the URL and text of the cloned tile
     clonedTile.href = link;
-    console.log("a veranderd");
     clonedTile.querySelector('p').innerText = text;
-    console.log("p veranderd");
     const Ielement = clonedTile.querySelector('i');
     Ielement.classList.remove("fa-thumbs-down");
-    Ielement.classList.add(icon);
-    console.log("i veranderd");
+    Ielement.classList.remove("fa-solid");
+    Ielement.classList.add(icondata);
+    Ielement.classList.add(icontype);
 
     // Append the cloned tile to the container
     document.getElementById('container').appendChild(clonedTile);
@@ -33,10 +44,17 @@ function remove_the_placeholder() {
 }
 function new_tile(text,icon,url) {
     //note the icon is using font awsom SIEM edision
-    data = JSON.parse(localStorage.getItem("main"))
-    data.naam
+    data = get_JSON();
+    data.naam.push(text);
+    data.icon.push(icon);
+    data.link.push(url);
+    localStorage.setItem("main",JSON.stringify(data));
 
+}
+function get_JSON(){
+    data = JSON.parse(localStorage.getItem("main"));
+    return data;
 }
 function setup() {
     // set the base json
-    if(localStorage.length === 0) {localStorage.setItem("main",'{"gebruiker":{"naam":"NAAM"},"naam":["google"],"link":["https://google.com"],"icon":["fa-thumbs-up"]}' );}}
+    if(localStorage.length === 0) {localStorage.setItem("main",'{"gebruiker":{"naam":"NAAM"},"naam":["google"],"link":["https://google.com"],"icon":{"icondata":["fa-thumbs-up"],"icontype":["fa-solid"]}}' );}}
